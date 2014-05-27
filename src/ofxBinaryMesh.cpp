789787ec3a5061d0cdc1e9ac58f87cdd88b082dd
Elie Zananiri <prisonerjohn@gmail.com>
2014-05-27 09:58:01 -0400

    Optimized copy by going directly into the ofMesh vectors.

--- src/ofxBinaryMesh.cpp
+++ src/ofxBinaryMesh.cpp
@@ -56,46 +56,36 @@ namespace ofx
             int numVerts = 0;
             file.read((char *)(&numVerts), sizeof(int));
             if (numVerts > 0) {
-                ofPoint * verts = new ofPoint[numVerts];
-                file.read((char *)(&verts[0]), sizeof(ofPoint) * numVerts);
-                mesh.addVertices(verts, numVerts);
-                delete [] verts;
+                mesh.getVertices().resize(numVerts);
+                file.read((char *)(&(mesh.getVertices())[0]), sizeof(ofPoint) * numVerts);
             }
 
             int numNormals = 0;
             file.read((char *)(&numNormals), sizeof(int));
             if (numNormals > 0) {
-                ofPoint * normals = new ofPoint[numNormals];
-                file.read((char *)(&normals[0]), sizeof(ofPoint) * numNormals);
-                mesh.addNormals(normals, numNormals);
-                delete [] normals;
+                mesh.getNormals().resize(numNormals);
+                file.read((char *)(&(mesh.getNormals())[0]), sizeof(ofPoint) * numNormals);
             }
 
             int numTexCoords = 0;
             file.read((char *)(&numTexCoords), sizeof(int));
             if (numTexCoords > 0) {
-                ofVec2f * texCoords = new ofVec2f[numTexCoords];
-                file.read((char *)(&texCoords[0]), sizeof(ofVec2f) * numTexCoords);
-                mesh.addTexCoords(texCoords, numTexCoords);
-                delete [] texCoords;
+                mesh.getTexCoords().resize(numTexCoords);
+                file.read((char *)(&(mesh.getTexCoords())[0]), sizeof(ofVec2f) * numTexCoords);
             }
 
             int numColors = 0;
             file.read((char *)(&numColors), sizeof(int));
             if (numColors > 0) {
-                ofFloatColor * colors = new ofFloatColor[numColors];
-                file.read((char *)(&colors[0]), sizeof(ofFloatColor) * numColors);
-                mesh.addColors(colors, numColors);
-                delete [] colors;
+                mesh.getColors().resize(numColors);
+                file.read((char *)(&(mesh.getColors())[0]), sizeof(ofFloatColor) * numColors);
             }
 
             int numIndices = 0;
             file.read((char *)(&numIndices), sizeof(int));
             if (numIndices > 0) {
-                ofIndexType * indices = new ofIndexType[numIndices];
-                file.read((char *)(&indices[0]), sizeof(ofIndexType) * numIndices);
-                mesh.addIndices(indices, numIndices);
-                delete [] indices;
+                mesh.getIndices().resize(numIndices);
+                file.read((char *)(&(mesh.getIndices())[0]), sizeof(ofIndexType) * numIndices);
             }
 
             file.close();
